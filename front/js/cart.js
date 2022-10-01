020f7bba79e264c28c9a4ae5a80c2eb967be6ba4
laurent <soyezlaurent@gmail.com>
2022-10-01 19:18:43 +0200

    modification du fectch + ajout creation balise

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,24 +1,19 @@
-let arrayCart = JSON.parse(localStorage.getItem("productCart"));
-console.log(arrayCart);
+async function fetchProduct() {
+  return await fetch(`http://localhost:3000/api/products`)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function (product) {
+      return product;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
 
-function generateCart() {
-  arrayCart.forEach(function (test) {
-    let recup = test.productId;
-    async function fetchProduct() {
-      return fetch(`http://localhost:3000/api/products/${recup}`)
-        .then(function (response) {
-          if (response.ok) {
-            console.log(response);
-            return response.json();
-          }
-        })
-        .then(function (data) {
-          return data;
-        });
-    }
-    console.log(fetchProduct);
-    console.log(recup);
-  });
+function generateCart(arrayCart) {
   for (let index = 0; index < arrayCart.length; index++) {
     const sectionCartItem = document.querySelector("#cart__items");
 
@@ -31,14 +26,67 @@ function generateCart() {
     article.appendChild(divImg);
 
     const img = document.createElement("img");
-    img.src = arrayCart.imageUrl;
-    img.alt = arrayCart.altTxt;
+    img.src = arrayCart[index].imageUrl;
+    img.alt = arrayCart[index].altTxt;
     divImg.appendChild(img);
+
+    const divItemContent = document.createElement("div");
+    divItemContent.classList.add("cart__item__content");
+    article.appendChild(divItemContent);
+
+    const divItemContentDescription = document.createElement("div");
+    divItemContentDescription.classList.add("cart__item__content__description");
+    divItemContent.appendChild(divItemContentDescription);
+
+    const title = document.createElement("h2");
+    title.innerHTML = arrayCart[index].name;
+    divItemContentDescription.appendChild(title);
+
+    const color = document.createElement("p");
+    color.innerHTML = arrayCart[index].color;
+    divItemContentDescription.appendChild(color);
+
+    const price = document.createElement("p");
+    price.innerHTML = arrayCart[index].price + " €";
+    divItemContentDescription.appendChild(price);
+
+    const divSettings = document.createElement("div");
+    divSettings.classList.add("cart__item__content__settings");
+    divItemContent.appendChild(divSettings);
+
+    const divQuantity = document.createElement("div");
+    divQuantity.classList.add("cart__item__content__settings__quantity");
+    divSettings.appendChild(divQuantity);
+
+    const pQuantity = document.createElement("p");
+    pQuantity.innerHTML = "Qté : " + arrayCart[index].quantity;
+    divQuantity.appendChild(pQuantity);
+
+    /* const input = document.createElement("input");
+    input.setAttribute */
   }
 }
 
 async function main() {
-  const product = await fetchProduct();
-  generateCart(product);
+  let productsInCart = JSON.parse(localStorage.getItem("productCart"));
+  const products = await fetchProduct();
+  let filteredProducts = [];
+  productsInCart.forEach(function (productInCart) {
+    products.forEach(function (product) {
+      if (productInCart.productId === product._id) {
+        filteredProducts.push({
+          productId: productInCart.productId,
+          name: product.name,
+          imageUrl: product.imageUrl,
+          altTxt: product.altTxt,
+          price: product.price,
+          color: productInCart.color,
+          quantity: productInCart.quantity,
+        });
+      }
+    });
+  });
+
+  generateCart(filteredProducts);
 }
 main();
